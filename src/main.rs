76ca22e2b201a256a377f6d9028b0b36662db34c
Johan Lindblad <johanlindblad@gmail.com>
2016-06-12 23:02:52 +0200

    Use bitfield

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
+#![feature(plugin)]
+#![allow(dead_code)]
+#![plugin(bitfield)]
+
 extern crate byteorder;
 
 use std::io::prelude::*;
 use std::fs::File;
-use std::io::Cursor;
 
-use byteorder::{BigEndian, ReadBytesExt};
+bitfield!{Instruction,
+    operator: 4,
+    orthography_a: 3,
+    other: 13,
+    register_a: 4,
+    register_b: 4,
+    register_c: 4
+}
 
-fn read_program(path: &str) -> Vec<u32> {
+fn read_program(path: &str) -> Vec<Instruction> {
     let mut file = File::open(path).unwrap();
 
-    let mut bytes: Vec<u8> = Vec::new();
-    file.read_to_end(&mut bytes).unwrap();
-    let mut buffer = Cursor::new(&bytes[..]);
-    let mut program: Vec<u32> = Vec::new();
+    let mut program: Vec<Instruction> = Vec::new();
+    let mut temp: [u8; 4] = [0; 4];
 
-    while let Ok(u32_value) = buffer.read_u32::<BigEndian>() {
-        program.push(u32_value);
+    while let Ok(4) = file.read(&mut temp) {
+        let instruction = Instruction::new(temp);
+        program.push(instruction);
     }
 
     program
@@ -25,5 +34,5 @@ fn main() {
     let program = read_program("../cmu.um");
 
     println!("Program length: {} 32-bit values", program.len());
-    println!("First byte: {}", program[0]);
+    println!("First byte: {}", program[0].get_operator());
 }
